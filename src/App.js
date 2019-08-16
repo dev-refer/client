220c8b0f53c3c62467aa5fd1f9543bef39026b6d
hafrizresa <hafrizresa@gmail.com>
2019-08-16 20:02:06 +0700

    skeleton

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import React, { Component } from 'react';
-import Base from './components/Base.jsx'
+import Login from './pages/login/Login.jsx';
+import Dashboard from './pages/dashboard/dashboard.jsx';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { PrivateRoute } from './privateRoute';
 
 // import './App.css';
 
 class App extends Component {
   render() {
-    return(
-        <div className="App">
-          <Base/>
-        </div>
+    return (
+      <div className="App">
+        <Router>
+          <Switch>
+            <Route path='/' component={Login} exact />
+            <PrivateRoute
+            path='/dashboard'
+            component={Dashboard}
+            />
+            <Route component={() => <h1>404 not found</h1>} />
+          </Switch>
+        </Router>
+      </div>
     )
   }
 }
